9181d8482401118c70ac3c8f47c2a6f3419eba23
setu <setu6211yuki@gmail.com>
2018-08-18 05:09:31 +0000

    kadai add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  befor_action :set_task, only: [:show, :edit, :update, destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+ 
   def index
     @tasks = Task.all
   end
@@ -47,10 +48,10 @@ class TasksController < ApplicationController
   private
   
   # Strong Parameter
-  def set_task
+  def set_task 
     @task = Task.find(params[:id])
   end
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180815095254) do
+ActiveRecord::Schema.define(version: 20180818044859) do
 
   create_table "tasklists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180815095254) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
